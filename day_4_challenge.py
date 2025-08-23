d5e734e01848241bb995f81fb2654a757af97e37
saurabhack <kamanesaurabh68@gmail.com>
2025-08-23 17:34:15 +0530

    fourth day of programming challenge is completed

---
+++ day_4_challenge.py
@@ -0,0 +1,19 @@
+# import random
+# freinds=['Alice','bob','charlie','david','emanuel']
+# index=random.randint(0,4)
+# print(freinds[index]) 
+#most preferable option 
+# print(random.choice(freinds))
+
+# print(len(freinds))
+# num_of_friends=len(freinds)
+# print(freinds[num_of_friends-1])
+
+
+dirty_dozens=["Strawberries","Spinach","Kale","Nectarines","Apples","Grapes","Peaches","Cherries","Pears","Tomatoes","Celery","Potatoes"]
+
+
+fruits=["something","something","something","something"]
+vagetables=["mot","mot","mot","mot"]
+dirty_dozens=[fruits,vagetables]
+print(dirty_dozens[1][1])

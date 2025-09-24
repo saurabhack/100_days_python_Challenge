8d01753910bf62f3a6a773c4a6d35911cc18e871
saurabh kamane <kamanesaurabh68@gmail.com>
2025-09-24 22:20:33 +0530

    something

---
+++ solution_walkthroug_15_day.py
@@ -0,0 +1,53 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+profit=0
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+
+is_on=True
+
+while True:
+    choice = input("What would you like? (expresson/latte/cappuciono)")
+    if choice == "off":
+        is_on=False
+    elif choice=="report":
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffee: {resources['coffee']}g")
+        print(f"money: ${profit}")
+    else:
+        drink=MENU[choice]
+        is_resource_sufficient(drink)
+        
